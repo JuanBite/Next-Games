4228f69e30004ab41177616723548e21113c3587
Juan David Quinchia <quinchiaj7@gmail.com>
2026-04-09 17:09:06 -0500

    Fix: Games page

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,20 +1,24 @@
 import { stackServerApp } from "@/stack/server";
-import { redirect } from "next/navigation"
+import { redirect } from "next/navigation";
 import SideBar from "@/components/SideBar";
-import GamesInfo from "@/components/GamesInfo"
+import GamesInfo from "@/components/GamesInfo";
 
-// games/page.tsx
-export default async function GamesPage({ searchParams } : { searchParams: Promise<{q?: string}> }) {
+export default async function GamesPage({
+    searchParams,
+}: {
+    searchParams?: {
+        q?: string;
+        page?: string;
+    };
+}) {
     const user = await stackServerApp.getUser();
-    if(!user) {
-        redirect('/');
+    if (!user) {
+        redirect("/");
     }
 
     return (
-        <div>
-            <SideBar currentPath={'/games'}>
-                <GamesInfo searchParams={searchParams} />
-            </SideBar>
-        </div>
+        <SideBar currentPath="/games">
+            <GamesInfo searchParams={searchParams} />
+        </SideBar>
     );
 }
